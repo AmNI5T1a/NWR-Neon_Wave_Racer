94a2c5ecc0396efb8d26ecd78bd04b7635e95e38
egitarbathwater <jeynado_farbi_ocimio_eceterial_j@yahoo.com>
2021-03-17 12:21:06 -0700

    Started working on road shop menu
    - Added some sprites like gears and arrow
    - Added event system to lobby

--- Assets/Scripts/GameModules/LobbyOnly/LobbyManager.cs
+++ Assets/Scripts/GameModules/LobbyOnly/LobbyManager.cs
@@ -11,10 +11,41 @@ namespace NWR
         [Header("References")]
         [SerializeField] private InputManager _inputManager;
 
-
+        [Header("In play mode settings")]
+        [SerializeField] private bool selectRoadMenuClosed = true;
         void Awake()
         {
             _inputManager.inputLocked = true;
+            selectRoadMenuClosed = true;
+        }
+
+
+        public void CheckConditionsAndStartAGame()
+        {
+
+        }
+
+        public void OpenOrCloseSelectRoadMenu()
+        {
+            if (selectRoadMenuClosed)
+                OpenSelectRoadMenu();
+            else if (!selectRoadMenuClosed)
+                CloseSelectRoadMenu();
+        }
+
+        public void OpenSelectRoadMenu()
+        {
+
+        }
+
+        public void CloseSelectRoadMenu()
+        {
+
+        }
+
+        public void TestDebugMessage()
+        {
+            Debug.Log("Button works well");
         }
     }
 }
