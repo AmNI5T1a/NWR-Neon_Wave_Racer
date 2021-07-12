64587ade959602adb833ea6c23d25d0ec35a6b63
Tima <jeynado_farbi_ocimio_eceterial_j@yahoo.com>
2021-07-12 08:47:40 -0700

    UI , Car
    - Big changes in UI
    - New mesh (Delorian 80s)
    - Gradient

---
+++ Assets/Scripts/UIGradient/SetDirty.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace NWR.UIGradient
+{
+    public class SetDirty : MonoBehaviour
+    {
+        public Graphic m_graphic;
+        // Use this for initialization
+        void Reset()
+        {
+            m_graphic = GetComponent<Graphic>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            m_graphic.SetVerticesDirty();
+        }
+    }
+}
